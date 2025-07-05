9d97b46c2bdeefeac1cca86a28969fb9a2bf1c78
Anjali <anjaligupta3406@gmail.com>
2025-07-05 23:44:47 +0530

    signup and logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,11 @@
+import { useAuthStore } from "../store/useAuthStore"
+
 const Navbar = () => {
+  const {logout,authUser}=useAuthStore();
   return (
-    <div>Navbar</div>
+    <>
+
+    </>
   )
 }
 export default Navbar
