655e83ae593e3be83ea5754cf6bcda9a25adeee0
Anjali <anjaligupta3406@gmail.com>
2025-07-10 15:22:48 +0530

    Fix route order to prevent path-to-regexp error on Render

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,9 +3,8 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 const router=express.Router();
 
-router.get("/users",protectRoute,getUsersForSidebar
-)
-router.get("/:id",protectRoute,getMessages)
+router.get("/:id", protectRoute, getMessages);
+router.post("/:id", protectRoute, sendMessage);
+router.get("/users", protectRoute, getUsersForSidebar);
 
-router.post("/:id",protectRoute,sendMessage)
 export default router;
